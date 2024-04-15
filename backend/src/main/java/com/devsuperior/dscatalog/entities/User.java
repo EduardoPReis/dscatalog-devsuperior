0b20b3f12d1b024ba4c57b091f6966e8e9a9c6d0
unknown <reisprogramer@gmail.com>
2024-04-15 20:28:10 -0300

    ORM mapping, database seeding

--- backend/src/main/java/com/devsuperior/dscatalog/entities/User.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/User.java
@@ -4,24 +4,41 @@ import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable{
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private String firstNme;
+	private String firstName;
 	private String lastName;
 	private String email;
 	private String password;
 	
+	@ManyToMany
+	@JoinTable(name ="tb_user_role",
+			   joinColumns = @JoinColumn(name = "user_id"),
+			   inverseJoinColumns = @JoinColumn(name = "role_id"))
 	private Set<Role> roles = new HashSet<>();
 	
 	public User() {
 	}
 
-	public User(Long id, String firstNme, String lastName, String email, String password) {
+	public User(Long id, String firstName, String lastName, String email, String password) {
 		super();
 		this.id = id;
-		this.firstNme = firstNme;
+		this.firstName = firstName;
 		this.lastName = lastName;
 		this.email = email;
 		this.password = password;
@@ -36,11 +53,11 @@ public class User implements Serializable{
 	}
 
 	public String getFirstNme() {
-		return firstNme;
+		return firstName;
 	}
 
 	public void setFirstNme(String firstNme) {
-		this.firstNme = firstNme;
+		this.firstName = firstNme;
 	}
 
 	public String getLastName() {
