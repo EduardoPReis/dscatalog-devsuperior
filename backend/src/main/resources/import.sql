60b166e758b431ec7f9c8e110a5b77d7734b622e
EduardoReis <reisprogramer@gmail.com>
2025-10-20 18:50:04 -0300

    Post Commit

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -2,3 +2,4 @@ INSERT INTO tb_category (name) VALUES ('Books');
 INSERT INTO tb_category (name) VALUES ('Electronics');
 INSERT INTO tb_category (name) VALUES ('Computers');
 INSERT INTO tb_category (name) VALUES ('Garden');
+INSERT INTO tb_category (name) VALUES ('SmarthPhones');
