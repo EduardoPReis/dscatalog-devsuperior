7ce1b7f28b46bd766e7d12054a8cb58a53f165dd
EduardoReis <reisprogramer@gmail.com>
2025-10-20 19:08:42 -0300

    Update Category

--- backend/src/main/java/com/eduardo/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/eduardo/dscatalog/services/CategoryService.java
@@ -39,4 +39,16 @@ public class CategoryService {
 		entity = repository.save(entity);
 		return new CategoryDTO(entity);
 	}
+
+	@Transactional
+	public CategoryDTO update(Long id,CategoryDTO dto) {
+		try {
+		Category entity = repository.getOne(id);
+		entity.setName(dto.getName());
+		entity = repository.save(entity);
+		return new CategoryDTO(entity);
+		}catch(EntityNotFoundException e) {
+			throw new EntityNotFoundException("id not found "+id);
+		}
+	}
 }
