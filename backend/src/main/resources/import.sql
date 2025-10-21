e8b9c62322ec594c0be27d5dd10189b5be7a4fbd
EduardoReis <reisprogramer@gmail.com>
2025-10-21 18:32:22 -0300

    category find pageable

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -3,3 +3,45 @@ INSERT INTO tb_category (name, created_at) VALUES ('Electronics',NOW());
 INSERT INTO tb_category (name, created_at) VALUES ('Computers',NOW());
 INSERT INTO tb_category (name, created_at) VALUES ('Garden',NOW());
 INSERT INTO tb_category (name, created_at) VALUES ('SmarthPhones',NOW());
+
+INSERT INTO tb_category (name, created_at) VALUES ('Books',NOW());
+INSERT INTO tb_category (name, created_at) VALUES ('Electronics',NOW());
+INSERT INTO tb_category (name, created_at) VALUES ('Computers',NOW());
+INSERT INTO tb_category (name, created_at) VALUES ('Garden',NOW());
+INSERT INTO tb_category (name, created_at) VALUES ('SmarthPhones',NOW());
+
+INSERT INTO tb_category (name, created_at) VALUES ('Books',NOW());
+INSERT INTO tb_category (name, created_at) VALUES ('Electronics',NOW());
+INSERT INTO tb_category (name, created_at) VALUES ('Computers',NOW());
+INSERT INTO tb_category (name, created_at) VALUES ('Garden',NOW());
+INSERT INTO tb_category (name, created_at) VALUES ('SmarthPhones',NOW());
+
+INSERT INTO tb_category (name, created_at) VALUES ('Books',NOW());
+INSERT INTO tb_category (name, created_at) VALUES ('Electronics',NOW());
+INSERT INTO tb_category (name, created_at) VALUES ('Computers',NOW());
+INSERT INTO tb_category (name, created_at) VALUES ('Garden',NOW());
+INSERT INTO tb_category (name, created_at) VALUES ('SmarthPhones',NOW());
+
+INSERT INTO tb_category (name, created_at) VALUES ('Books',NOW());
+INSERT INTO tb_category (name, created_at) VALUES ('Electronics',NOW());
+INSERT INTO tb_category (name, created_at) VALUES ('Computers',NOW());
+INSERT INTO tb_category (name, created_at) VALUES ('Garden',NOW());
+INSERT INTO tb_category (name, created_at) VALUES ('SmarthPhones',NOW());
+
+INSERT INTO tb_category (name, created_at) VALUES ('Books',NOW());
+INSERT INTO tb_category (name, created_at) VALUES ('Electronics',NOW());
+INSERT INTO tb_category (name, created_at) VALUES ('Computers',NOW());
+INSERT INTO tb_category (name, created_at) VALUES ('Garden',NOW());
+INSERT INTO tb_category (name, created_at) VALUES ('SmarthPhones',NOW());
+
+INSERT INTO tb_category (name, created_at) VALUES ('Books',NOW());
+INSERT INTO tb_category (name, created_at) VALUES ('Electronics',NOW());
+INSERT INTO tb_category (name, created_at) VALUES ('Computers',NOW());
+INSERT INTO tb_category (name, created_at) VALUES ('Garden',NOW());
+INSERT INTO tb_category (name, created_at) VALUES ('SmarthPhones',NOW());
+
+INSERT INTO tb_category (name, created_at) VALUES ('Books',NOW());
+INSERT INTO tb_category (name, created_at) VALUES ('Electronics',NOW());
+INSERT INTO tb_category (name, created_at) VALUES ('Computers',NOW());
+INSERT INTO tb_category (name, created_at) VALUES ('Garden',NOW());
+INSERT INTO tb_category (name, created_at) VALUES ('SmarthPhones',NOW());
